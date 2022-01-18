486754ee91da60972a4468d0a4829d23b2fcc256
Nikita Yudin <nikitayudin782@gmail.com>
2022-01-18 15:39:47 +0300

    change CommonJS on ECMAScript Import/Export
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/index.js
+++ server/src/index.js
@@ -7,8 +7,7 @@ import app from './app.js'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 dotenv.config({ path: __dirname + '/modules/config/.env' })
 
