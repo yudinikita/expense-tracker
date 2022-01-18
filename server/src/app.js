7b7c99229ce2b38c77a5d14e610a097273a8492a
Nikita Yudin <nikitayudin782@gmail.com>
2022-01-18 17:03:17 +0300

    change dirname
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/app.js
+++ server/src/app.js
@@ -4,8 +4,11 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import morgan from 'morgan'
 import serveStatic from 'serve-static'
-import path from 'path'
+import path, { dirname } from 'path'
 import corsOptions from './modules/config/cors.js'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express().disable('x-powered-by')
 
@@ -20,12 +23,12 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  const pathStatic = path.join(import.meta.url, 'client', 'build')
+  const pathStatic = path.join(__dirname, 'client', 'build')
   const index = 'index.html'
   app.use('/', serveStatic(pathStatic, { index }))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(import.meta.url, 'client', 'build', index))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', index))
   })
 }
 
