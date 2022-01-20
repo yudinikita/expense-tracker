16af80250377942369d663f50255216f6825831e
Nikita Yudin <nikitayudin782@gmail.com>
2022-01-20 23:53:33 +0300

    fix connect db
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/config/db.ts
+++ server/src/modules/config/db.ts
@@ -1,24 +1,24 @@
+import colors from 'ansi-colors'
 import mongoose from 'mongoose'
 
+const NameDB: string = colors.magenta('[MongoDB] ')
+
 const connectDB = async (app: any): Promise<void> => {
-  const MONGO_URI = process.env['MONGO_URI'] || ''
+  const MONGO_URI: string = process.env['MONGO_URI'] || ''
 
   try {
     mongoose.connection.on('connected', () => {
-      app.log.info({ actor: 'MongoDB' }, 'connected')
+      app.log.info(NameDB + colors.green('connected ✔'))
     })
 
     mongoose.connection.on('disconnected', () => {
-      app.log.error({ actor: 'MongoDB' }, 'disconnected')
+      app.log.error(NameDB + colors.red('disconnected ✘'))
     })
 
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+    await mongoose.connect(MONGO_URI)
   } catch (error: any) {
-    app.log.error({ actor: 'MongoDB' }, 'error')
-    app.log.error({ actor: 'MongoDB' }, error?.message)
+    app.log.error(NameDB + 'error')
+    app.log.error(NameDB, error?.message)
     process.exit(1)
   }
 }
