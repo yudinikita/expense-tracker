46db884a285e583d13d5c789a2e49fc59bdc4929
Nikita Yudin <nikitayudin782@gmail.com>
2022-01-21 16:11:37 +0300

    add fastify-jwt and refactoring fastify static
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/app.js
+++ server/src/app.js
@@ -1,36 +1,25 @@
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
 import fastify from 'fastify'
 import fastifyCompress from 'fastify-compress'
 import fastifyStatic from 'fastify-static'
 import fastifyCors from 'fastify-cors'
 import fastifyHelmet from 'fastify-helmet'
+import fastifyJwt from 'fastify-jwt'
 import corsOptions from './modules/config/cors.js'
 import fastifyOptions from './modules/config/fastifyOptions.js'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
+import jwtOptions from './modules/config/jwt.js'
+import staticOptions, { staticRoute } from './modules/config/static.js'
 
 const buildApp = async () => {
   const app = fastify(fastifyOptions)
 
   app.register(fastifyCompress)
-
   app.register(fastifyCors, corsOptions)
-
   app.register(fastifyHelmet)
+  app.register(fastifyJwt, jwtOptions)
 
-  if (process.env['NODE_ENV'] === 'production') {
-    const pathStatic = path.join(__dirname, 'client', 'build')
-    const index = 'index.html'
-
-    app.register(fastifyStatic, {
-      root: path.join(pathStatic, index),
-      prefix: '/'
-    })
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', index))
-    })
+  if (process.env.NODE_ENV === 'production') {
+    app.register(fastifyStatic, staticOptions)
+    app.route(staticRoute)
   }
 
   return app
