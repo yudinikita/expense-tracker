8a3ebed6e8c5bf122e805d92193b70f20a719f30
Nikita Yudin <nikitayudin782@gmail.com>
2022-01-22 00:19:04 +0300

    refactor(server:config) divide into separate files from static
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

---
+++ server/src/modules/config/static/static.ts
@@ -0,0 +1,35 @@
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FastifyReply, FastifyRequest, RouteOptions } from 'fastify'
+
+const _dirname = dirname(fileURLToPath(import.meta.url))
+
+const STATIC_CONFIG = {
+  PATHS: {
+    CLIENT: 'client',
+    BUILD: 'build',
+    FILE_NAME: 'index.html'
+  },
+  PREFIX: '/',
+  ROUTE: {
+    URL: '*'
+  }
+}
+
+const pathStatic = path.join(_dirname, STATIC_CONFIG.PATHS.CLIENT, STATIC_CONFIG.PATHS.BUILD)
+const root = path.join(pathStatic, STATIC_CONFIG.PATHS.FILE_NAME)
+
+export const staticOptions = {
+  root,
+  prefix: STATIC_CONFIG.PREFIX
+}
+
+async function handler (_request: FastifyRequest, reply: FastifyReply): Promise<void> {
+  await reply.sendFile(STATIC_CONFIG.PATHS.FILE_NAME, pathStatic)
+}
+
+export const staticRoute: RouteOptions = {
+  method: 'GET',
+  url: STATIC_CONFIG.ROUTE.URL,
+  handler
+}
