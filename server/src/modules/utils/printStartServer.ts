ab0ff8ce58a77db584848b2cf7329f795bdd0f21
Nikita Yudin <nikitayudin782@gmail.com>
2022-01-22 00:20:29 +0300

    refactor(server:config) move printStartServer.ts to utils from config folder
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/utils/printStartServer.ts
+++ server/src/modules/utils/printStartServer.ts
@@ -1,19 +1,19 @@
 import colors from 'ansi-colors'
 import constants from '../constants/constants.js'
 
-const printStartServer = async (PORT, HOSTNAME) => {
-  const NODE_ENV = process.env.NODE_ENV || constants.DEFAULT_NODE_ENV
-  const CLIENT_URL = process.env.CLIENT_URL || ''
+const printStartServer = async (PORT: string | number, HOSTNAME: string): Promise<void> => {
+  const NODE_ENV = process.env['NODE_ENV'] ?? constants.DEFAULT_NODE_ENV
+  const CLIENT_URL = process.env['CLIENT_URL'] ?? ''
+  const SERVER_URL = `http://${HOSTNAME}:${PORT}`
   const PAD_START = 10
-  const TYPE_SERVER = 'http://'
 
   console.log(colors.yellow('[ExpenseTracker] ') + colors.bold('AccessURLs:'))
   console.log('--------------------------------------')
-  console.log('Server: '.padStart(PAD_START) + TYPE_SERVER + colors.magenta(HOSTNAME) + ':' + PORT)
+  console.log('Server: '.padStart(PAD_START) + SERVER_URL)
   console.log('Client: '.padStart(PAD_START) + colors.magenta(CLIENT_URL))
   console.log('--------------------------------------')
   console.log('Mode: '.padStart(PAD_START) + colors.magenta(NODE_ENV))
-  console.log('Port: '.padStart(PAD_START) + colors.magenta(PORT.toString()))
+  console.log('Port: '.padStart(PAD_START) + colors.magenta(String(PORT)))
   console.log('--------------------------------------')
 }
 
