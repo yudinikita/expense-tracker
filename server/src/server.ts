63e93b456d4f332c8e03c1f7c31c94c4711b16b6
Nikita Yudin <nikitayudin782@gmail.com>
2022-01-21 17:24:28 +0300

    fix server.js
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,14 @@
-import colors from 'ansi-colors'
 import { ApolloServer } from 'apollo-server-fastify'
 import buildApp from './app.js'
 import connectDB from './modules/config/db.js'
 import apolloConfig from './modules/config/apollo.js'
+import printStartServer from './modules/config/printStartServer.js'
+import constants from './modules/constants/constants.js'
+import { FastifyInstance } from 'fastify'
 
-const DEFAULT_PORT = 5000
-
-const startServer = async () => {
-  const NODE_ENV = process.env['NODE_ENV'] || ''
-
-  const PORT = process.env['PORT'] || DEFAULT_PORT
-
-  const HOSTNAME = process.env['HOSTNAME'] || ''
-  const TYPE_SERVER = 'http://'
-
-  const CLIENT_URL = process.env['CLIENT_URL'] || ''
-
-  const PAD_START = 10
+const startServer = async (): Promise<FastifyInstance> => {
+  const PORT = process.env['PORT'] ?? constants.SERVER.DEFAULT_PORT
+  const HOSTNAME = process.env['HOSTNAME'] ?? constants.SERVER.DEFAULT_HOST
 
   const app = await buildApp()
 
@@ -25,22 +17,17 @@ const startServer = async () => {
 
     const apolloServer = new ApolloServer(apolloConfig)
     await apolloServer.start()
-    app.register(apolloServer.createHandler({ cors: false }))
+    await app.register(apolloServer.createHandler({ cors: false }))
 
     await app.listen(PORT, HOSTNAME)
 
-    console.log(colors.yellow('[ExpenseTracker] ') + colors.bold('AccessURLs:'))
-    console.log('--------------------------------------')
-    console.log('Server: '.padStart(PAD_START) + TYPE_SERVER + colors.magenta(HOSTNAME) + ':' + PORT)
-    console.log('Client: '.padStart(PAD_START) + colors.magenta(CLIENT_URL))
-    console.log('--------------------------------------')
-    console.log('Mode: '.padStart(PAD_START) + colors.magenta(NODE_ENV))
-    console.log('Port: '.padStart(PAD_START) + colors.magenta(PORT.toString()))
-    console.log('--------------------------------------')
+    await printStartServer(PORT, HOSTNAME)
   } catch (error) {
     app.log.error(error)
-    process.exit(1)
+    process.exit(0)
   }
+
+  return app
 }
 
 export default startServer
