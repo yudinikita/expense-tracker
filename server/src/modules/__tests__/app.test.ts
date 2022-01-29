352726360e94ac1ef1d74598f05073efe64fa82f
Nikita Yudin <nikitayudin782@gmail.com>
2022-01-29 22:22:53 +0300

    test: add for main app
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

---
+++ server/src/modules/__tests__/app.test.ts
@@ -0,0 +1,31 @@
+import buildApp from '../app'
+import whitelist from '../config/cors/whitelist.js'
+import { FastifyInstance } from 'fastify'
+
+describe('App', () => {
+
+  let app: FastifyInstance
+  const allowOrigin: string = whitelist[0]!
+  const notAllowOrigin: string = 'not-allow-origin'
+
+  beforeAll(async () => {
+    app = await buildApp()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('should return status code 200 when app is ready', async () => {
+    const response = await app.inject({ url: '/', headers: { origin: allowOrigin } })
+
+    expect(response.statusCode).toBe(200)
+  })
+
+  test('should return status code 500 when cors not allowed', async () => {
+    const response = await app.inject({ url: '/', headers: { origin: notAllowOrigin } })
+
+    expect(response.statusCode).toBe(500)
+  })
+
+})
