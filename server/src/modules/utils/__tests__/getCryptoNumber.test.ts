98bd6e7b31c70f22a355db2e26c27d41a3bd3d29
Nikita Yudin <nikitayudin782@gmail.com>
2022-01-29 23:41:37 +0300

    test: remove async/await
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/utils/__tests__/getCryptoNumber.test.ts
+++ server/src/modules/utils/__tests__/getCryptoNumber.test.ts
@@ -2,8 +2,8 @@ import getCryptoNumber from '../getCryptoNumber.js'
 
 describe('Utils - crypto number:', () => {
 
-  it('should return typeof number', async () => {
-    const randomNum = await getCryptoNumber(5, 10)
+  it('should return typeof number', () => {
+    const randomNum = getCryptoNumber(5, 10)
 
     expect(randomNum).toBeDefined()
     expect(typeof randomNum).toBe('number')
