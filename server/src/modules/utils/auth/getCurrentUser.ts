421cdd44e2aeb5b8c7ff1ea8bc1cb7c69849e989
Nikita Yudin <nikitayudin782@gmail.com>
2022-01-30 00:23:27 +0300

    refactor: return null to catch
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/utils/auth/getCurrentUser.ts
+++ server/src/modules/utils/auth/getCurrentUser.ts
@@ -1,19 +1,18 @@
 import { validateAccessToken } from '../../service/token/index.js'
 
 export const getCurrentUser = async (authHeader: string): Promise<object | null> => {
-  let authToken = ''
   let currentUser: object | null = null
 
   try {
     const authHeaderArr = authHeader.split(' ')
     const typeAuth = authHeaderArr[0] ?? ''
-    authToken = authHeaderArr[1] ?? ''
+    const authToken = authHeaderArr[1] ?? ''
 
     if (typeAuth === 'Bearer') {
       currentUser = await validateAccessToken(authToken)
     }
   } catch (e) {
-    console.warn(`Unable to authenticate using auth token: ${authToken}`)
+    return null
   }
 
   return currentUser
