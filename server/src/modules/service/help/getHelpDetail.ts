17011d45c58f629f28ca105fc39a9ac0b3904cf7
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:53:47 +0300

    refactor: update service
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/help/getHelpDetail.ts
+++ server/src/modules/service/help/getHelpDetail.ts
@@ -1,13 +1,13 @@
 import pkg from 'mongoose'
 import { HelpModel } from '../../models/index.js'
-import { User } from '../../graphql/__generated__/graphql.types.gen.js'
+import { Help, HelpDetailInput, User } from '../../graphql/__generated__/graphql.types.gen.js'
 
 const { Types } = pkg
 
-export const getHelpDetail = async (user: User, helpId: string): Promise<any> => {
-  const id = new Types.ObjectId(helpId)
+export const getHelpDetail = async (user: User, input: HelpDetailInput): Promise<Help | null> => {
+  const id = new Types.ObjectId(input.id)
 
-  const helpFetched = await HelpModel.findOne({ user, _id: id })
+  const helpFetched = await HelpModel.findOne({ user: user.id, _id: id })
 
   if (helpFetched === null) return null
 
