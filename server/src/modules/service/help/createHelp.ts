fd90f50f6aab77e7817f57742fd425d1a50b7d6a
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:53:39 +0300

    refactor: update service
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/help/createHelp.ts
+++ server/src/modules/service/help/createHelp.ts
@@ -1,13 +1,13 @@
 import pkg from 'mongoose'
 import { HelpModel } from '../../models/index.js'
-import { HelpInput, User } from '../../graphql/__generated__/graphql.types.gen.js'
+import { CreateHelpInput, User } from '../../graphql/__generated__/graphql.types.gen.js'
 
 const { Types } = pkg
 
-export const createHelp = async (user: User, help?: HelpInput): Promise<any> => {
+export const createHelp = async (user: User, input: CreateHelpInput): Promise<any> => {
   const userId = new Types.ObjectId(user.id)
 
-  const newHelp = new HelpModel({ ...help, user: userId })
+  const newHelp = new HelpModel({ ...input, user: userId })
 
   await newHelp.save()
 
