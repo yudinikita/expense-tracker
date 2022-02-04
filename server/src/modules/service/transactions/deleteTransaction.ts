8c8229422312dfe911c0796a6b2cca36871a8bc2
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:54:18 +0300

    refactor: update service
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/transactions/deleteTransaction.ts
+++ server/src/modules/service/transactions/deleteTransaction.ts
@@ -1,22 +1,19 @@
-import pkg from 'mongoose'
 import { TransactionModel } from '../../models/index.js'
+import { DeleteTransactionInput } from '../../graphql/__generated__/graphql.types.gen.js'
+import { toObjectId } from '../../utils/index.js'
 
-const { Types } = pkg
-
-export const deleteTransaction = async (id: string): Promise<any> => {
-  const transactionId = new Types.ObjectId(id)
-  const deletedTransaction = await TransactionModel.deleteOne({ _id: transactionId })
+export const deleteTransaction = async (input: DeleteTransactionInput): Promise<any> => {
+  const deletedTransaction = await TransactionModel.deleteOne({ _id: toObjectId(input.id) })
 
   const isDeleted = deletedTransaction.deletedCount === 1
+
   if (isDeleted) {
     return {
-      code: 200,
       success: true,
       message: 'Операция удалена'
     }
   } else {
     return {
-      code: 200,
       success: false,
       message: 'Операция не удалена'
     }
