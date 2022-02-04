db6e9279be9b4adcf6f35685d41f391c9faa7b81
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:49:38 +0300

    test: add utils for test user input
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

---
+++ server/src/modules/utils/test/user/testUserInput.ts
@@ -0,0 +1,6 @@
+import { UserRegistrationInput } from '../../../graphql/__generated__/graphql.types.gen.js'
+
+export const testUserInput: UserRegistrationInput = {
+  email: 'test@mail.com',
+  password: 'TestUser_password123'
+}
