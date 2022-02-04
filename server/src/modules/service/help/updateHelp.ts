37a93d24af337860b9ab5b7239156503772163c5
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:53:54 +0300

    refactor: update service
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/help/updateHelp.ts
+++ server/src/modules/service/help/updateHelp.ts
@@ -1,9 +1,21 @@
 import { HelpModel } from '../../models/index.js'
-import { HelpUpdate } from '../../graphql/__generated__/graphql.types.gen.js'
+import { Help, UpdateHelpInput } from '../../graphql/__generated__/graphql.types.gen.js'
 
-export const updateHelp = async (helpUpdate: HelpUpdate): Promise<any> => {
-  return await HelpModel.findOneAndUpdate(
-    { _id: helpUpdate.id },
-    { $set: { solved: helpUpdate.solved } }
+export const updateHelp = async (helpUpdate: UpdateHelpInput): Promise<Help> => {
+  const updatedHelp = await HelpModel.findByIdAndUpdate(
+    helpUpdate.id,
+    { $set: { solved: helpUpdate.solved } },
+    { returnDocument: 'after' }
   )
+
+  return {
+    id: updatedHelp?._id,
+    user: updatedHelp?.user,
+    title: updatedHelp?.title,
+    detail: updatedHelp?.detail,
+    solved: updatedHelp?.solved,
+    answer: updatedHelp?.answer,
+    createdAt: updatedHelp?.createdAt,
+    updatedAt: updatedHelp?.updatedAt
+  }
 }
