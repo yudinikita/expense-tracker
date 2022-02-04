8fa9ea239286e5919f419c0354ed4b2dbbab5ac7
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:38:04 +0300

    refactor: change type
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/database/types/queryAnalyticsBalance.type.ts
+++ server/src/modules/database/types/queryAnalyticsBalance.type.ts
@@ -1,5 +1,5 @@
-import pkg from 'mongoose'
-import { AnalyticsCategory, Balance } from '../../graphql/__generated__/graphql.types.gen.js'
+import { AnalyticsCategory } from '../../graphql/__generated__/graphql.types.gen.js'
+import mongoose from 'mongoose'
 
 export interface ResponseAnalytics {
   income: number
@@ -7,21 +7,13 @@ export interface ResponseAnalytics {
 }
 
 export type QueryAnalytics = (
-  userId: pkg.Types.ObjectId,
-  gte: Date,
-  lte: Date
+  userId: mongoose.Types.ObjectId,
+  gte?: Date | undefined,
+  lte?: Date | undefined
 ) => Promise<ResponseAnalytics>
 
 export type QueryAnalyticsCategory = (
-  userId: pkg.Types.ObjectId,
-  gte: Date,
-  lte: Date
+  userId: mongoose.Types.ObjectId,
+  gte?: Date | undefined,
+  lte?: Date | undefined
 ) => Promise<AnalyticsCategory[]>
-
-export type QueryBalance = (userId: pkg.Types.ObjectId) => Promise<Balance[]>
-
-export type QueryBalancePerDate = (
-  userId: pkg.Types.ObjectId,
-  gte: Date,
-  lte: Date
-) => Promise<Balance[]>
