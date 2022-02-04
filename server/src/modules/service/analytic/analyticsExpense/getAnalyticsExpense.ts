57e28b44761580bfb77ee5d70f342fe3d9180707
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:52:25 +0300

    refactor: update service
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/analytic/analyticsExpense/getAnalyticsExpense.ts
+++ server/src/modules/service/analytic/analyticsExpense/getAnalyticsExpense.ts
@@ -1,31 +1,10 @@
-import pkg from 'mongoose'
 import { AnalyticsCategory, AnalyticsInput, User } from '../../../graphql/__generated__/graphql.types.gen.js'
 import { queryAnalyticsExpense } from '../../../database/index.js'
+import { toObjectId } from '../../../utils/index.js'
 
-const { Types } = pkg
+export const getAnalyticsExpense = async (user: User, input?: AnalyticsInput | null): Promise<AnalyticsCategory[]> => {
+  const gte = input?.filter?.date?.gte ? new Date(input.filter.date.gte) : undefined
+  const lte = input?.filter?.date?.lte ? new Date(input.filter.date.lte) : undefined
 
-export const getAnalyticsExpense = async (user: User, input: AnalyticsInput): Promise<AnalyticsCategory[]> => {
-  const userId = new Types.ObjectId(user?.id)
-
-  let analyticsCategory: AnalyticsCategory[] = []
-
-  const shouldApplyFilters = input?.filter
-
-  if (shouldApplyFilters == null) return analyticsCategory
-
-  const shouldApplyDateFilters = input?.filter?.date
-
-  if (shouldApplyDateFilters != null) {
-    const gteFilter = input?.filter?.date?.gte ?? null
-    const lteFilter = input?.filter?.date?.lte ?? null
-
-    if (gteFilter !== null && lteFilter !== null) {
-      const gte = new Date(gteFilter)
-      const lte = new Date(lteFilter)
-
-      analyticsCategory = await queryAnalyticsExpense(userId, gte, lte)
-    }
-  }
-
-  return analyticsCategory
+  return await queryAnalyticsExpense(toObjectId(user.id), gte, lte)
 }
