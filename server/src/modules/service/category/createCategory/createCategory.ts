5d1001ff1977040835a984a0aa4cc1ab2a304dd9
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 18:45:58 +0300

    refactor: add convert to json
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/category/createCategory/createCategory.ts
+++ server/src/modules/service/category/createCategory/createCategory.ts
@@ -1,6 +1,8 @@
 import { CategoryModel } from '../../../models/index.js'
 import { Category, CategoryInput, User } from '../../../graphql/__generated__/graphql.types.gen.js'
+import { toObjectId } from '../../../utils/index.js'
 
 export const createCategory = async (user: User, category: CategoryInput): Promise<Category> => {
-  return await CategoryModel.create({ user: user.id, title: category.title })
+  const newCategory = await CategoryModel.create({ user: toObjectId(user.id), title: category.title })
+  return newCategory.toJSON()
 }
