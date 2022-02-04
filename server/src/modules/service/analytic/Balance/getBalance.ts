fc35d078ff3ef69bb914eab34976dc8c5337f30c
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:52:47 +0300

    refactor: update service
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/analytic/Balance/getBalance.ts
+++ server/src/modules/service/analytic/Balance/getBalance.ts
@@ -1,40 +1,36 @@
-import pkg from 'mongoose'
 import { AnalyticsInput, Balance, User } from '../../../graphql/__generated__/graphql.types.gen.js'
-import { queryBalance, queryBalancePerDate } from '../../../database/index.js'
+import { TransactionModel } from '../../../models/index.js'
+import { toObjectId } from '../../../utils/index.js'
 
-const { Types } = pkg
+export const getBalance = async (user: User, input?: AnalyticsInput | null): Promise<Balance> => {
+  const gte = input?.filter?.date?.gte ? new Date(input.filter.date.gte) : undefined
+  const lte = input?.filter?.date?.lte ? new Date(input.filter.date.lte) : undefined
 
-export const getBalance = async (user: User, input: AnalyticsInput): Promise<any> => {
-  const userId = new Types.ObjectId(user?.id)
-
-  let balance: Balance = {
-    count: 0,
-    totalAmount: 0
-  }
-
-  const shouldApplyFilters = input?.filter
-
-  if (shouldApplyFilters == null) {
-    const balanceFetched = await queryBalance(userId)
-
-    if (balanceFetched[0] != null) balance = balanceFetched[0]
-  } else {
-    const shouldApplyDateFilters = input?.filter?.date
-
-    if (shouldApplyDateFilters != null) {
-      console.log('filter')
-      const gteFilter = input?.filter?.date?.gte ?? null
-      const lteFilter = input?.filter?.date?.lte ?? null
-
-      if (gteFilter !== null && lteFilter !== null) {
-        const gte = new Date(gteFilter)
-        const lte = new Date(lteFilter)
-
-        const balanceFetched = await queryBalancePerDate(userId, gte, lte)
-        if (balanceFetched[0] != null) balance = balanceFetched[0]
+  const balance = await TransactionModel.aggregate([
+    {
+      $match: {
+        user: toObjectId(user.id),
+        createdAt: {
+          $gte: gte,
+          $lte: lte
+        }
+      }
+    },
+    {
+      $group: {
+        _id: '$user',
+        totalAmount: { $sum: '$amount' },
+        count: { $sum: 1 }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        totalAmount: 1,
+        count: 1
       }
     }
-  }
+  ])
 
-  return balance
+  return balance[0]
 }
