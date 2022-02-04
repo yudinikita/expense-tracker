a40411d7a441325317f69a510ae22872b60361dc
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:51:57 +0300

    refactor: update service
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/analytic/analyticsAverage/getAnalyticsAverage.ts
+++ server/src/modules/service/analytic/analyticsAverage/getAnalyticsAverage.ts
@@ -1,11 +1,10 @@
-import pkg from 'mongoose'
 import { AnalyticsBalance, AnalyticsInput, User } from '../../../graphql/__generated__/graphql.types.gen.js'
 import { queryAnalyticsAverage } from '../../../database/index.js'
+import { toObjectId } from '../../../utils/index.js'
 
-const { Types } = pkg
-
-export const getAnalyticsAverage = async (user: User, input: AnalyticsInput): Promise<AnalyticsBalance> => {
-  const userId = new Types.ObjectId(user?.id)
+export const getAnalyticsAverage = async (user: User, input?: AnalyticsInput | null): Promise<AnalyticsBalance> => {
+  const gte = input?.filter?.date?.gte ? new Date(input.filter.date.gte) : undefined
+  const lte = input?.filter?.date?.lte ? new Date(input.filter.date.lte) : undefined
 
   const analyticsBalance: AnalyticsBalance = {
     income: 0,
@@ -13,27 +12,11 @@ export const getAnalyticsAverage = async (user: User, input: AnalyticsInput): Pr
     remainder: 0
   }
 
-  const shouldApplyFilters = input?.filter
+  const transactionsIncome = await queryAnalyticsAverage(toObjectId(user.id), gte, lte)
 
-  if (shouldApplyFilters == null) return analyticsBalance
-
-  const shouldApplyDateFilters = input?.filter?.date
-
-  if (shouldApplyDateFilters != null) {
-    const gteFilter = input?.filter?.date?.gte ?? null
-    const lteFilter = input?.filter?.date?.lte ?? null
-
-    if (gteFilter !== null && lteFilter !== null) {
-      const gte = new Date(gteFilter)
-      const lte = new Date(lteFilter)
-
-      const transactionsIncome = await queryAnalyticsAverage(userId, gte, lte)
-
-      analyticsBalance.income = Math.round(transactionsIncome.income)
-      analyticsBalance.expense = Math.round(Math.abs(transactionsIncome.expanse))
-      analyticsBalance.remainder = Math.round(transactionsIncome.income - Math.abs(transactionsIncome.expanse))
-    }
-  }
+  analyticsBalance.income = Math.round(transactionsIncome.income)
+  analyticsBalance.expense = Math.round(Math.abs(transactionsIncome.expanse))
+  analyticsBalance.remainder = Math.round(transactionsIncome.income - Math.abs(transactionsIncome.expanse))
 
   return analyticsBalance
 }
