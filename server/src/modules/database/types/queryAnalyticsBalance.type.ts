abc95dc1736b3fe65698290078a7916931d73c39
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 18:43:15 +0300

    refactor: update query type
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/database/types/queryAnalyticsBalance.type.ts
+++ server/src/modules/database/types/queryAnalyticsBalance.type.ts
@@ -1,4 +1,4 @@
-import { AnalyticsCategory } from '../../graphql/__generated__/graphql.types.gen.js'
+import { AnalyticsCategory, AnalyticsInput } from '../../graphql/__generated__/graphql.types.gen.js'
 import mongoose from 'mongoose'
 
 export interface ResponseAnalytics {
@@ -8,8 +8,7 @@ export interface ResponseAnalytics {
 
 export type QueryAnalytics = (
   userId: mongoose.Types.ObjectId,
-  gte?: Date | undefined,
-  lte?: Date | undefined
+  input?: AnalyticsInput | null
 ) => Promise<ResponseAnalytics>
 
 export type QueryAnalyticsCategory = (
