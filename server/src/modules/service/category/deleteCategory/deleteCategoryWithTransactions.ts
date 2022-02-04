933a636946054fd65855c0e92b71255599b9100d
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:53:21 +0300

    refactor: update service
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/category/deleteCategory/deleteCategoryWithTransactions.ts
+++ server/src/modules/service/category/deleteCategory/deleteCategoryWithTransactions.ts
@@ -1,13 +1,18 @@
 import pkg from 'mongoose'
 import { CategoryModel } from '../../../models/index.js'
-import { CategoryDeleteInput } from '../../../graphql/__generated__/graphql.types.gen.js'
+import { CategoryDeleteInput, CategoryDeletePayload } from '../../../graphql/__generated__/graphql.types.gen.js'
 import { deleteManyTransaction } from '../../transactions/index.js'
 
 const { Types } = pkg
 
-export const deleteCategoryWithTransactions = async (input: CategoryDeleteInput): Promise<boolean> => {
+export const deleteCategoryWithTransactions = async (input: CategoryDeleteInput): Promise<CategoryDeletePayload> => {
   const categoryId = new Types.ObjectId(input.id)
   const result = await CategoryModel.deleteOne({ _id: categoryId })
   await deleteManyTransaction(categoryId)
-  return result.deletedCount === 1
+  const success = result.deletedCount === 1
+
+  return {
+    id: input.id,
+    success
+  }
 }
