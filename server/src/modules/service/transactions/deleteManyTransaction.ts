9f7b25d612d00808178431122066fec0b266466f
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:54:12 +0300

    refactor: update service
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/transactions/deleteManyTransaction.ts
+++ server/src/modules/service/transactions/deleteManyTransaction.ts
@@ -1,7 +1,7 @@
 import { TransactionModel } from '../../models/index.js'
 import { Types } from 'mongoose'
 
-export const deleteManyTransaction = async (categoryId: Types.ObjectId) => {
+export const deleteManyTransaction = async (categoryId: Types.ObjectId): Promise<boolean> => {
   const result = await TransactionModel.deleteMany({ category: categoryId })
   return result.deletedCount === 1
 }
