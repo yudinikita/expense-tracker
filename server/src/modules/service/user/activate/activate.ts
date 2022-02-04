fa5924e7241080c052dfbc99effd4e152ac6c07f
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:55:00 +0300

    refactor: update service
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/user/activate/activate.ts
+++ server/src/modules/service/user/activate/activate.ts
@@ -1,17 +1,20 @@
 import pkg from 'mongoose'
 import { UserModel } from '../../../models/index.js'
 import { generateToken } from '../../token/index.js'
+import { User } from '../../../graphql/__generated__/graphql.types.gen.js'
+import { AuthenticationError } from 'apollo-server-fastify'
+import constants from '../../../constants/constants.js'
 
 const { Types } = pkg
 
-export const activate = async (id: string, activationCode: string): Promise<any> => {
+export const activate = async (id: string, activationCode: string): Promise<User> => {
   const userId = new Types.ObjectId(id)
   const user: any = await UserModel.findOne({ _id: userId })
 
-  if (user === null) throw new Error('Ошибка авторизации')
+  if (user === null) throw new AuthenticationError(constants.GRAPHQL.MESSAGE.AUTH_ERROR)
 
   if (user.activationCode !== activationCode) {
-    throw new Error('Некорректный код активации')
+    throw new Error(constants.GRAPHQL.MESSAGE.ACTIVATION_CODE_ERROR)
   }
 
   user.isActivated = true
@@ -20,5 +23,11 @@ export const activate = async (id: string, activationCode: string): Promise<any>
 
   const accessToken = await generateToken({ ...user })
 
-  return { accessToken, user }
+  return {
+    id: user.id,
+    email: user.email,
+    isActivated: user.isActivated,
+    settings: user.settings,
+    accessToken
+  }
 }
