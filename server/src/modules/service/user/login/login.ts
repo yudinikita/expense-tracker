291efb544410940c1a3285b16e48c2790b90d902
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:55:09 +0300

    refactor: update service
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/user/login/login.ts
+++ server/src/modules/service/user/login/login.ts
@@ -1,21 +1,36 @@
 import { UserModel } from '../../../models/index.js'
 import { getPasswordConfirmation } from '../../../utils/auth/index.js'
 import { generateToken } from '../../token/index.js'
-import { UserLoginInput } from '../../../graphql/__generated__/graphql.types.gen.js'
+import { User } from '../../../graphql/__generated__/graphql.types.gen.js'
+import constants from '../../../constants/constants.js'
 
-export const login = async (email: string, password: string): Promise<any> => {
-  const user: UserLoginInput | null = await UserModel.findOne({ email })
+export const login = async (email: string, password: string): Promise<User> => {
+  const user: User & { password: string } | null = await UserModel.findOne({ email })
 
   if (user === null) {
-    throw new Error(`Пользователь с почтовым адресом ${email} не найден`)
+    throw new Error(constants.GRAPHQL.MESSAGE.LOGIN_ERROR)
   }
 
   const isPassConfirm = await getPasswordConfirmation(password, user.password)
+
   if (!isPassConfirm) {
-    throw new Error('Неверный пароль')
+    throw new Error(constants.GRAPHQL.MESSAGE.LOGIN_ERROR)
   }
 
-  const accessToken = await generateToken({ ...user })
+  const userDto = {
+    // @ts-expect-error
+    ...user.toJSON(),
+    // @ts-expect-error
+    id: user._id
+  }
 
-  return { accessToken, user }
+  const accessToken = await generateToken({ ...userDto })
+
+  return {
+    id: user.id,
+    email: user.email,
+    isActivated: user.isActivated,
+    settings: user.settings,
+    accessToken
+  }
 }
