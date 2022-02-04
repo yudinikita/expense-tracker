4259e1b124aeabad2dd529ad67accefee50b260e
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:54:04 +0300

    refactor: update service
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/transactions/createTransaction.ts
+++ server/src/modules/service/transactions/createTransaction.ts
@@ -1,22 +1,19 @@
 import { TransactionModel } from '../../models/index.js'
 import { TransactionInput } from '../../graphql/__generated__/graphql.types.gen.js'
-import pkg from 'mongoose'
+import { toObjectId } from '../../utils/index.js'
 
-const { Types } = pkg
+export const createTransaction = async (userId: string, input: TransactionInput): Promise<any> => {
+  const newTransaction = await new TransactionModel({ ...input, user: toObjectId(userId) })
 
-export const createTransaction = async (id: string, transaction?: TransactionInput): Promise<any> => {
-  const userId = new Types.ObjectId(id)
-
-  const newTransaction = new TransactionModel({ ...transaction, user: userId })
   await newTransaction.save()
+
   const populateTransaction = await newTransaction.populate('category')
 
-  const createdAt = new Date(populateTransaction._doc.createdAt).toISOString()
-  const updatedAt = new Date(populateTransaction._doc.updatedAt).toISOString()
+  const createdAt = new Date(populateTransaction.createdAt).toISOString()
+  const updatedAt = new Date(populateTransaction.updatedAt).toISOString()
 
   return {
-    ...populateTransaction._doc,
-    _id: populateTransaction.id,
+    ...populateTransaction.toJSON(),
     createdAt,
     updatedAt
   }
