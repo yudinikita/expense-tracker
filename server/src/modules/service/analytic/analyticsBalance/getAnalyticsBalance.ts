b5f8b79087234c573706611d61a64541068d6859
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:52:06 +0300

    refactor: update service
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/analytic/analyticsBalance/getAnalyticsBalance.ts
+++ server/src/modules/service/analytic/analyticsBalance/getAnalyticsBalance.ts
@@ -1,11 +1,10 @@
-import pkg from 'mongoose'
 import { AnalyticsBalance, AnalyticsInput, User } from '../../../graphql/__generated__/graphql.types.gen.js'
 import { queryAnalyticsBalance } from '../../../database/index.js'
+import { toObjectId } from '../../../utils/index.js'
 
-const { Types } = pkg
-
-export const getAnalyticsBalance = async (user: User, input: AnalyticsInput): Promise<AnalyticsBalance> => {
-  const userId = new Types.ObjectId(user?.id)
+export const getAnalyticsBalance = async (user: User, input?: AnalyticsInput | null): Promise<AnalyticsBalance> => {
+  const gte = input?.filter?.date?.gte ? new Date(input.filter.date.gte) : undefined
+  const lte = input?.filter?.date?.lte ? new Date(input.filter.date.lte) : undefined
 
   const analyticsBalance: AnalyticsBalance = {
     income: 0,
@@ -13,26 +12,11 @@ export const getAnalyticsBalance = async (user: User, input: AnalyticsInput): Pr
     remainder: 0
   }
 
-  const shouldApplyFilters = input?.filter
-  if (shouldApplyFilters == null) return analyticsBalance
+  const transactionsIncome = await queryAnalyticsBalance(toObjectId(user.id), gte, lte)
 
-  const shouldApplyDateFilters = input?.filter?.date
-
-  if (shouldApplyDateFilters != null) {
-    const gteFilter = input?.filter?.date?.gte ?? null
-    const lteFilter = input?.filter?.date?.lte ?? null
-
-    if (gteFilter !== null && lteFilter !== null) {
-      const gte = new Date(gteFilter)
-      const lte = new Date(lteFilter)
-
-      const transactionsIncome = await queryAnalyticsBalance(userId, gte, lte)
-
-      analyticsBalance.income = transactionsIncome.income
-      analyticsBalance.expense = Math.abs(transactionsIncome.expanse)
-      analyticsBalance.remainder = transactionsIncome.income - Math.abs(transactionsIncome.expanse)
-    }
-  }
+  analyticsBalance.income = transactionsIncome.income
+  analyticsBalance.expense = Math.abs(transactionsIncome.expanse)
+  analyticsBalance.remainder = transactionsIncome.income - Math.abs(transactionsIncome.expanse)
 
   return analyticsBalance
 }
