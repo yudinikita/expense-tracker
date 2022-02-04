052a5f8e70be9a3e5de0fa436ec3002bed90b474
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-04 15:53:13 +0300

    refactor: update service
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/service/category/deleteCategory/deleteCategoryReplace.ts
+++ server/src/modules/service/category/deleteCategory/deleteCategoryReplace.ts
@@ -1,11 +1,16 @@
 import { CategoryModel } from '../../../models/index.js'
-import { CategoryDeleteInput } from '../../../graphql/__generated__/graphql.types.gen.js'
+import { CategoryDeleteInput, CategoryDeletePayload } from '../../../graphql/__generated__/graphql.types.gen.js'
 import { updateTransactionsByCategory } from '../../transactions/index.js'
 
-export const deleteCategoryReplace = async (input: CategoryDeleteInput): Promise<boolean> => {
+export const deleteCategoryReplace = async (input: CategoryDeleteInput): Promise<CategoryDeletePayload> => {
   const id = input.id
   const newCategoryId = input.newId ?? ''
   await updateTransactionsByCategory(id, newCategoryId)
   const result = await CategoryModel.deleteOne({ _id: id })
-  return result.deletedCount === 1
+  const success = result.deletedCount === 1
+
+  return {
+    id: input.id,
+    success
+  }
 }
