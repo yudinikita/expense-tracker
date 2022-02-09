639fb22ad26555bc6bcfaca294b950f25e49c30c
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-09 12:49:44 +0300

    refactor: update line
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/config/cors/cors.ts
+++ server/src/modules/config/cors/cors.ts
@@ -1,5 +1,5 @@
 import { FastifyCorsOptions } from 'fastify-cors'
-//import { origin } from './origin.js'
+// import { origin } from './origin.js'
 
 export const corsOptions: FastifyCorsOptions = {
   credentials: true
