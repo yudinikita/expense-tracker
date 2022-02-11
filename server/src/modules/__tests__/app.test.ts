613b980c5aeab99681ed2608b2bdbd626f76211b
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-11 16:49:20 +0300

    test: remove test for cors not allow
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/__tests__/app.test.ts
+++ server/src/modules/__tests__/app.test.ts
@@ -6,7 +6,6 @@ describe('App', () => {
 
   let app: FastifyInstance
   const allowOrigin: string = whitelist[0]!
-  const notAllowOrigin: string = 'not-allow-origin'
 
   beforeAll(async () => {
     app = await buildApp({})
@@ -20,12 +19,6 @@ describe('App', () => {
     const response = await app.inject({ url: '/', headers: { origin: allowOrigin } })
 
     expect(response.statusCode).toBe(200)
-  })
-
-  it('should return status code 500 when cors not allowed', async () => {
-    const response = await app.inject({ url: '/', headers: { origin: notAllowOrigin } })
-
-    expect(response.statusCode).toBe(500)
   })
 
 })
