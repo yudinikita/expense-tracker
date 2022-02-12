e4a74c34d844ac4a60aad57ea66803be51d6a082
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-12 17:29:57 +0300

    refactor: convert to ts
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- client/src/modules/routes/RequireAuth.tsx
+++ client/src/modules/routes/RequireAuth.tsx
@@ -5,8 +5,9 @@ import { Navigate, Outlet } from 'react-router-dom'
 export const RequireAuth = () => {
   const isAuthenticated = useIsAuthenticated()
   const auth = useAuthUser()
+  const user = auth()
 
-  const isActivated = auth()?.isActivated
+  const isActivated = user?.['isActivated']
 
   if (!isAuthenticated()) return <Navigate to={'/start'} />
   if (!isActivated) return <Navigate to={'/activate'} />
