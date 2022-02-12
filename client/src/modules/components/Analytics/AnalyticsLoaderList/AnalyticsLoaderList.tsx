806556902b0fa84e7dc2085425e543cc33c18d67
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-12 22:19:32 +0300

    refactor: convert to ts
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- client/src/modules/components/Analytics/AnalyticsLoaderList/AnalyticsLoaderList.tsx
+++ client/src/modules/components/Analytics/AnalyticsLoaderList/AnalyticsLoaderList.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ContentLoader from 'react-content-loader'
 
-export const AnalyticsLoaderList = (props) => (
+export const AnalyticsLoaderList: React.FC = (props) => (
   <ContentLoader
     speed={2}
     width={315}
