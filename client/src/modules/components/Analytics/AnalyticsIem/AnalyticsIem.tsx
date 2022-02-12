e1a33823692c7b1b05e8ac1dd33d455710a4e83d
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-12 22:19:07 +0300

    refactor: convert to ts
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- client/src/modules/components/Analytics/AnalyticsIem/AnalyticsIem.tsx
+++ client/src/modules/components/Analytics/AnalyticsIem/AnalyticsIem.tsx
@@ -1,28 +1,30 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import PropTypes, { InferProps } from 'prop-types'
 import { LineProgressBar, Price } from '../../index'
 import styles from './AnalyticsItem.module.scss'
+
+const propTypes = {
+  title: PropTypes.string,
+  amount: PropTypes.number,
+  percent: PropTypes.number,
+  color: PropTypes.string
+}
 
 export const AnalyticsItem = ({
   title,
   amount,
   percent,
   color
-}) => {
+}: InferProps<typeof propTypes>) => {
   return (
     <>
       <div className={styles.info}>
         <p>{title}</p>
         <Price amount={amount} haveColor={false} />
       </div>
-      <LineProgressBar percent={percent} color={color} />
+      <LineProgressBar percent={percent ?? undefined} color={color} />
     </>
   )
 }
 
-AnalyticsItem.propTypes = {
-  title: PropTypes.string,
-  amount: PropTypes.number,
-  percent: PropTypes.number,
-  color: PropTypes.string
-}
+AnalyticsItem.propTypes = propTypes
