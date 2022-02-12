b7c003ba2df453a681fb5bd2c0e5fd71ad823caf
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-12 22:19:00 +0300

    refactor: convert to ts
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- client/src/modules/components/Analytics/AnalyticsExpense/AnalyticsExpense.tsx
+++ client/src/modules/components/Analytics/AnalyticsExpense/AnalyticsExpense.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import PropTypes, { InferProps } from 'prop-types'
 import { MyError } from '../../MyError'
 import { AnalyticsBalanceList, AnalyticsLoaderList } from '..'
 import { useAnalyticsExpense } from './hooks/useAnalyticsExpense'
 
 const propTypes = {
   date: PropTypes.exact({
-    activeDate: PropTypes.instanceOf(Date),
-    startDate: PropTypes.instanceOf(Date),
-    endDate: PropTypes.instanceOf(Date)
-  }),
+    activeDate: PropTypes.instanceOf(Date).isRequired,
+    startDate: PropTypes.instanceOf(Date).isRequired,
+    endDate: PropTypes.instanceOf(Date).isRequired
+  }).isRequired
 }
 
-export const AnalyticsExpense = ({ date }) => {
+export const AnalyticsExpense = ({ date }: InferProps<typeof propTypes>) => {
   const { analyticsItems, total, loading, error } = useAnalyticsExpense(date?.startDate, date?.endDate)
 
   if (loading) return <AnalyticsLoaderList />
