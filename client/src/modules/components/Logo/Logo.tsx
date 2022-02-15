571569944e7643d00f5438d00aff4a1326c0403b
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-15 19:04:38 +0300

    refactor: convert to ts
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- client/src/modules/components/Logo/Logo.tsx
+++ client/src/modules/components/Logo/Logo.tsx
@@ -3,7 +3,8 @@ import SVG from 'react-inlinesvg'
 
 const srcLogo = '/images/logo.svg'
 
-export const Logo = (props) => (
+export const Logo: React.FC<React.SVGProps<SVG>> = (props) => (
+  // @ts-expect-error
   <SVG
     src={srcLogo}
     title='Логотип Денежки'
