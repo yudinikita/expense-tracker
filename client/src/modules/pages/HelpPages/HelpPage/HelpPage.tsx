1079b7af13a4b2c9dbf8b0039fe870acafb6e352
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-15 19:08:46 +0300

    refactor: convert to ts
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- client/src/modules/pages/HelpPages/HelpPage/HelpPage.tsx
+++ client/src/modules/pages/HelpPages/HelpPage/HelpPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { HelpMain, InnerNavigate } from '../../../components'
 
-export const HelpPage = () => {
+export const HelpPage: React.FC = () => {
   return (
     <>
       <InnerNavigate title='Помощь' />
