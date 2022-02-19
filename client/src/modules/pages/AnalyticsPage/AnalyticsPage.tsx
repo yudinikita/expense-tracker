f5089f817a18f55dafbf3f4ee9cb7e7e749c431a
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-19 21:33:14 +0300

    refactor: add translation
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- client/src/modules/pages/AnalyticsPage/AnalyticsPage.tsx
+++ client/src/modules/pages/AnalyticsPage/AnalyticsPage.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { AnalyticsMain, PageTitle } from '../../components'
 
 import analytic from '../../assets/animation/analytic.json'
+import { useTranslation } from 'react-i18next'
 
 export const AnalyticsPage: React.FC = () => {
+  const { t } = useTranslation()
+
   return (
     <>
-      <PageTitle title='Аналитика' icon={analytic} />
+      <PageTitle title={t('analytics.title')} icon={analytic} />
       <AnalyticsMain />
     </>
   )
