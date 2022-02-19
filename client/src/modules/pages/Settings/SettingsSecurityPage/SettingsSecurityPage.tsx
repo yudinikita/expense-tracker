32f97c4abf20c41d34acd3ee28ca2b5a5dec6c96
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-19 21:33:52 +0300

    refactor: add translation
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- client/src/modules/pages/Settings/SettingsSecurityPage/SettingsSecurityPage.tsx
+++ client/src/modules/pages/Settings/SettingsSecurityPage/SettingsSecurityPage.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { InnerNavigate, SettingsSecurityMain } from '../../../components'
+import { useTranslation } from 'react-i18next'
 
 export const SettingsSecurityPage: React.FC = () => {
+  const { t } = useTranslation()
+
   return (
     <>
-      <InnerNavigate title='Безопасность' />
+      <InnerNavigate title={t('settings.security.title')} />
       <SettingsSecurityMain />
     </>
   )
