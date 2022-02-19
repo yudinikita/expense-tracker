1f6d54680f397edd1d056f3b2503761c65975a3a
Nikita Yudin <nikitayudin782@gmail.com>
2022-02-19 21:43:35 +0300

    refactor: add translation
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- client/src/modules/components/MainNavigation/components/NavItem.tsx
+++ client/src/modules/components/MainNavigation/components/NavItem.tsx
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom'
 import InlineSVG from 'react-inlinesvg'
 import styles from './NavItem.module.scss'
 import { MainLink } from '../../../data'
+import { useTranslation } from 'react-i18next'
 
 const pathIcon = '/images/icons/nav'
 
@@ -12,17 +13,21 @@ interface Props {
   link: MainLink
 }
 
-export const NavItem: React.FC<Props> = ({ link }) => (
-  <NavLink
-    to={`/${link.to}`}
-    title={link.title}
-    className={classNameLink}
-    end
-  >
-    <InlineSVG
-      className={styles.icon}
-      src={`${pathIcon}/${link.iconName}.svg`}
-      loader={<p>{link.loader}</p>}
-    />
-  </NavLink>
-)
+export const NavItem: React.FC<Props> = ({ link }) => {
+  const { t } = useTranslation()
+
+  return (
+    <NavLink
+      to={`/${link.to}`}
+      title={t(`navigation.list.${link.id}`)}
+      className={classNameLink}
+      end
+    >
+      <InlineSVG
+        className={styles.icon}
+        src={`${pathIcon}/${link.iconName}.svg`}
+        loader={<p>{link.loader}</p>}
+      />
+    </NavLink>
+  )
+}
