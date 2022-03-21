0b959ec8385dbc1dce33ada6ae37a433aaff6e37
Nikita Yudin <nikitayudin782@gmail.com>
2022-03-21 19:59:25 +0300

    feat: add i18n
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- server/src/modules/app.ts
+++ server/src/modules/app.ts
@@ -3,6 +3,9 @@ import fastifyCompress from 'fastify-compress'
 import fastifyStatic from 'fastify-static'
 import fastifyCors from 'fastify-cors'
 import fastifyHelmet from 'fastify-helmet'
+import i18next from 'i18next'
+import i18nextMiddleware from 'i18next-http-middleware'
+import { i18nOptions } from './config/i18n/options.js'
 import { corsOptions } from './config/cors/index.js'
 import { devRoute } from './config/fastify/index.js'
 import { staticOptions, staticRoute } from './config/static/static.js'
@@ -13,6 +16,10 @@ const buildApp = async (fastifyOptions: FastifyServerOptions): Promise<FastifyIn
   await app.register(fastifyCompress)
   await app.register(fastifyCors, corsOptions)
   await app.register(fastifyHelmet)
+
+  await i18next.use(i18nextMiddleware.LanguageDetector).init(i18nOptions)
+
+  await app.register(i18nextMiddleware.plugin, { i18next })
 
   if (process.env['NODE_ENV'] === 'production') {
     await app.register(fastifyStatic, staticOptions)
