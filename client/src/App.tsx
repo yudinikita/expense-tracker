a88469d97e11e5ddd258a4806d5d7f4db531546d
Nikita Yudin <nikitayudin782@gmail.com>
2022-03-21 20:25:29 +0300

    feat(apollo): rename apollo provider
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import { AuthProvider } from 'react-auth-kit'
 import { Provider as AlertProvider } from 'react-alert'
-import { MyApolloProvider } from './plugins/apollo'
-import { MyAlert, optionsAlert } from './modules/components/MyAlert'
-import { RouteComponent } from './modules/routes/routes'
+import { useTranslation } from 'react-i18next'
+import { MainApolloProvider } from 'plugins/apollo'
+import { AlertBox, optionsAlert } from 'modules/ui'
+import { RouteComponent } from 'modules/routes/routes'
 import './modules/styles/main.scss'
 
 export const App = () => {
+  const { t } = useTranslation()
+
+  document.title = t('page.title')
+
   return (
-    <MyApolloProvider>
+    <MainApolloProvider>
       <AuthProvider
         authType='localstorage'
         authName='_auth'
       >
-        {/* @ts-expect-error */}
-        <AlertProvider template={MyAlert} {...optionsAlert}>
+        <AlertProvider template={AlertBox} {...optionsAlert}>
           <RouteComponent />
         </AlertProvider>
       </AuthProvider>
-    </MyApolloProvider>
+    </MainApolloProvider>
   )
 }
--- client/src/plugins/apollo/apollo.provider.tsx
+++ client/src/plugins/apollo/apollo.provider.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ApolloProvider } from '@apollo/client'
 import { apolloClient } from './apollo.client'
 
-export const MyApolloProvider: React.FC = ({ children }) => (
+export const MainApolloProvider: React.FC = ({ children }) => (
   <ApolloProvider client={apolloClient}>
     {children}
   </ApolloProvider>
