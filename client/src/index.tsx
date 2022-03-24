e4318e372635df2b83a3e6c3670040a85bd7afd3
Nikita Yudin <nikitayudin782@gmail.com>
2022-03-24 20:16:31 +0300

    feat: add pwa
    Signed-off-by: Nikita Yudin <nikitayudin782@gmail.com>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Render } from 'Render'
+import * as serviceWorkerRegistration from './plugins/serviceWorker'
 import reportWebVitals from './plugins/reportWebVitals'
 import './plugins/i18next'
 import './plugins/dayjs'
@@ -8,4 +9,12 @@ import './plugins/dayjs'
 const $root = document.getElementById('root')
 ReactDOM.render(<Render />, $root)
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
